9d593a04ff24ab495522bc1ef250d9228919ec51
Nahid1911 <nahid.raihan@gmail.com>
2023-08-24 16:30:19 +0600

    Update Models and business logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,21 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: "User"
+  has_many :likes
+  has_many :comments
+
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
+
+  after_save :update_user_post_counter
+
+  def update_user_post_counter
+    author.update(post_counter: author.posts.count)
+  end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
 end
