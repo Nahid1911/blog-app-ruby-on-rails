8b32295b38b26406cab73e54052e42878b0d9967
Nahid1911 <nahid.raihan@gmail.com>
2023-08-25 10:48:20 +0600

    Add test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   attribute :photo, :string
 
   validates :name, presence: true
-  validates :post_counter, numericality: { only_integer: true, grater_than_or_equal_to: 0 }
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
