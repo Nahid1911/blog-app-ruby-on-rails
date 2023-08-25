f5c78a7622f09ff1c5d920971dbb094230974d5f
Nahid1911 <nahid.raihan@gmail.com>
2023-08-25 10:50:54 +0600

    Add test for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,9 @@ class Post < ApplicationRecord
   attribute :comments_counter, :integer, default: 0
   attribute :likes_counter, :integer, default: 0
 
-  validates :title, persence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: {only_integer: true, grater_than_or_equal_to: 0}
-  validates :likes_counter, numericality: {only_integer: true, grater_than_or_equal_to: 0}
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
   after_save :update_user_post_counter
 
