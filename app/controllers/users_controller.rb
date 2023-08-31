e43d0b5be1a8444ec5ae6ae710558aecd77b77f0
Nahid1911 <nahid.raihan@gmail.com>
2023-08-31 20:58:29 +0600

    Add wireframes with links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
   @users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @recent_posts = @user.three_most_recent_posts
+    @posts = Post.all
+    @post=Post.find params[:id]
+    @post_id = @posts.index(@post) + 1 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do
-    resources :posts , only:[:index,:show], controller: 'user_posts'
+    resources :posts, only: [:index, :show]
   end
 
-  resources :posts, only: [:index, :show]
+  # resources :posts, only: [:show]
   root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
