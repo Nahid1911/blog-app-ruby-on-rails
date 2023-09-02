6273c14ddffe8abbb8e87f2fe6f8bc29d3de9cf4
Nahid1911 <nahid.raihan@gmail.com>
2023-09-02 13:53:49 +0600

    Add likes methods and controler

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create]do
     resources :comments ,only:[:index,:new,:create]
-    resources :comments ,only:[:create, :destroy]
+    resources :likes ,only:[:create]
     end
   end
 
