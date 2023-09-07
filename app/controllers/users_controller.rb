5520fc80f812f23927ed80fe4ac15b65677049cc
techmoves <muhndlovu28@gmail.com>
2023-09-07 11:11:03 +0200

    Fix sign out error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
   end
 
   def show
+
+    if params[:id] == 'sign_out'
+      sign_out(current_user)
+      redirect_to new_user_session_path, notice: 'You have signed out.'
+    else
+
       @user = User.find(params[:id])
       @user_posts = @user.posts
       @recent_posts = @user_posts.limit(3)
