d0624250fd9a2c1f388268807524c19b82d75f7e
techmoves <muhndlovu28@gmail.com>
2023-09-07 11:13:04 +0200

    Fix linter error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   end
 
   def show
-
     if params[:id] == 'sign_out'
       sign_out(current_user)
       redirect_to new_user_session_path, notice: 'You have signed out.'
