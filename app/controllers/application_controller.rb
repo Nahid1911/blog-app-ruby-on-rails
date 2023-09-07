6939b9bcb27cf0947c145df1a0a52b96aaef4f9f
techmoves <muhndlovu28@gmail.com>
2023-09-07 10:29:14 +0200

    Fix linter error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   before_action :update_allowed_parameters, if: :devise_controller?
-  :after_sign_out_path_for
 
   private
 
@@ -13,13 +12,16 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-  def after_sign_out_path_for(resource_or_scope)
+
+  def after_sign_out_path_for(_resource_or_scope)
     # Replace '/your_custom_path' with the path you want to redirect to after sign-out
     '/users/sign_in'
   end
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :email, :password, :current_password)
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,12 @@ class UsersController < ApplicationController
 
   def show
     if params[:id] == 'sign_out'
-    sign_out(current_user)
-    redirect_to new_user_session_path, notice: 'You have signed out.'
+      sign_out(current_user)
+      redirect_to new_user_session_path, notice: 'You have signed out.'
     else
-    @user= User.find(params[:id])
-    @user_posts = @user.posts
-    @recent_posts = @user_posts.limit(3)
+      @user = User.find(params[:id])
+      @user_posts = @user.posts
+      @recent_posts = @user_posts.limit(3)
     end
   end
-
 end
