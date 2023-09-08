8a7bfa703f8ac79786ce44b902a1d9bf4ad233e1
Nahid1911 <nahid.raihan@gmail.com>
2023-09-09 00:01:24 +0600

    Fix sigh-out problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  load_and_authorize_resource
   before_action :authenticate_user!
 
   def index
@@ -12,11 +11,9 @@ class UsersController < ApplicationController
       sign_out(current_user)
       redirect_to new_user_session_path, notice: 'You have signed out.'
     else
-
       @user = User.find(params[:id])
       @user_posts = @user.posts
       @recent_posts = @user_posts.limit(3)
     end
   end
-  @post = Post.new
 end
