6806fba4fce6c6d3530d0b716aab26a836906e97
Nahid1911 <nahid.raihan@gmail.com>
2023-09-09 10:19:53 +0600

    Generate user api/v1/uses controller

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :posts, only: [:index]
+        resources :posts, only: [:index] do
+         resources :comments, only: [:index, :create]
+        end
       end
     end
   end
